4faf1f31094cbf64822d014793c8a87ec33e8c85
K-LoZ3 <carlos193zambra@gmail.com>
2021-04-08 16:01:12 -0500

    Hago el reto de la clase dentro del archivo index.js de la misma clase.

--- backend_node/movies-api/index.js
+++ backend_node/movies-api/index.js
@@ -13,7 +13,23 @@ app.get('/json', function(req, res) {
   res.json({ hello: 'world' });
 });
 
+// *********************************************************************
+// Funcion para ver si el año es divisible por 4, 100 o 400.
+function leapYear(year, div) {
+  return year % div === 0;
+}
+// Detectar bisiestos con parametro en la url.
+app.get('/leap/:year', function(req, res) {
+  const { year } = req.params;
+  if(leapYear(year, 4) && !leapYear(year, 100) || leapYear(year, 400)) {
+    res.send(`El año ${year} es bisiesto.`);
+  } else {
+    res.send(`El año ${year} no es bisiesto.`);
+  }
+});
+// *********************************************************************
+
 // Esto es para no quemar el puerto sino que lo traiga de config.
 app.listen(config.port, function() {
-    console.log('Listening http://localhost:', config.port);
+  console.log('Listening http://localhost:', config.port);
 });
